4e4c53ae5da2c9c06d01e6f777d49fb5c81014cd
Ozzee <maksim.nemna.dev@gmail>
2019-02-04 16:30:06 +0300

    added exception try

--- Rational/main.cpp
+++ Rational/main.cpp
@@ -167,5 +167,21 @@ int main()
 
     cout << "OK" << endl;
 
+    try {
+            Rational r(1, 0);
+            cout << "Doesn't throw in case of zero denominator" << endl;
+            return 1;
+    }
+    catch (invalid_argument &ex) {cout << ex.what()<< endl;};
+
+    try {
+        auto x = Rational(1, 2) / Rational(0, 1);
+        cout << "Doesn't throw in case of division by zero" << endl;
+        return 2;
+    }
+    catch (domain_error &ex) {cout << ex.what()<< endl;};
+
+        cout << "OK" << endl;
+
     return 0;
 }
